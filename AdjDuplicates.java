b72a8854e9bd63db3b5fbfe9ca0843dcdcb8fcb9
Aarush Mahajan <50150154+AarushMahajan@users.noreply.github.com>
2019-11-26 20:47:35 +0530

    Update AdjDuplicates.java

--- AdjDuplicates.java
+++ AdjDuplicates.java
@@ -1,32 +1,34 @@
+package github;
+
 import java.util.Scanner;
 class AdjDuplicates
 {
-static String a=" ";
-public static void fun(char [] s, int i)
- {
-
-if(i==0 && s[i]!=s[i+1])
-{
-a+=s[i];
-}
-else if(i==s.length-1)
-{
-if(s[i]!=s[i-1]) a+=s[i];
-return;
-}
-else if(s[i]!=s[i-1] && s[i]!=s[i+1])
-{
-a+=s[i];
-}
-fun(s,i+1);
-
-}
+static String s= "";
+	
+	public static void re_dup(char str[] , int i) {
+		
+		 if(i==str.length-1) {
+			 if(str[i]!=str[i-1]) {
+				 s=s+str[i];
+			 }
+				return ;
+			}
+		if(i==0) {
+			s=s+str[i];
+		}
+		if( i!=0 && str[i]!=str[i+1] && str[i]!=str[i-1]) {
+			s=s+str[i];
+		}
+		
+			re_dup(str, i+1);
+		
+	}
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 String str;
 str=sc.next();
-fun(str.toCharArray(),0);
+re_dup(str.toCharArray(),0);
 System.out.println(a);
 }
 }
