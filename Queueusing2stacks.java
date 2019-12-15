131a621227ea93d6cafc823cb4ff4b796971c9e7
Aarush Mahajan <50150154+AarushMahajan@users.noreply.github.com>
2019-12-15 13:51:33 +0530

    Update Queueusing2stacks.java

--- Queueusing2stacks.java
+++ Queueusing2stacks.java
@@ -5,7 +5,56 @@ public class Queueusing2stacks {
 	Node head;
 	static class Node{
 		int data;
-		Node next;
+		Node next;package queue;
+import java.util.*;
+
+public class QueueUsing2Stack {
+	
+	static Stack<Integer> s = new Stack<Integer>();
+	static Stack<Integer> s1 = new Stack<Integer>();
+	
+	public static void enqueue(int a) {
+		s.push(a);
+	}
+	public static void dequeue() {
+		
+		if(s1.empty()) {
+			while(!s.empty()) {
+				s1.push(s.pop());
+				}
+		}
+			
+			System.out.println(s1.pop());
+			
+			while(!s1.empty()) {
+				s.push(s1.pop());
+			}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		enqueue(1);
+		enqueue(2);
+		enqueue(3);
+		enqueue(4);
+		dequeue();
+		dequeue();
+		enqueue(5);
+		enqueue(6);
+		enqueue(7);
+		dequeue();
+		dequeue();
+		dequeue();
+		
+		
+		
+//		System.out.println(s1);
+		
+	}
+
+}
+
 		Node(int d){
 			data=d;
 			next=null;
